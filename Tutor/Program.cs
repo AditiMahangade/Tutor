a6df3ac7861869fa7cd62cff61fb9a12025b2447
Aditi Mahangade <Aditi.A.Mahangade-1@ou.edu>
2024-05-09 16:01:57 -0500

    Add project files.

---
+++ Tutor/Program.cs
@@ -0,0 +1,29 @@
+﻿// Aditi Mahangade
+// HW 3 Task 2 Tutor
+
+Random rnd = new Random();
+int num1 = rnd.Next(0, 51);
+int num2 = rnd.Next(0, 51);
+int answer = num1 + num2;
+int userInputInt;
+
+do
+{
+    string message = string.Format($"{num1}+{num2}=?");
+    Console.Write(message);
+    string userInputStr = Console.ReadLine();
+    userInputInt = Convert.ToInt32(userInputStr);
+    if (userInputInt == answer)
+    {
+        message = "Congratulations! You get the right answer!";
+        Console.WriteLine(message);
+        break;
+    }
+    else
+    {
+        message = "Sorry, please try again";
+        Console.WriteLine(message);
+    }
+}
+while (true);
+// while(userInputInt!=answer);
